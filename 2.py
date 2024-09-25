afa135daed3af54439a4c950b6102b659c9e5a9e
ipwnser <maxyxa782@gmail.com>
2024-09-25 08:36:21 +0300

    2.py

--- 2.py
+++ 2.py
@@ -3,7 +3,7 @@ import math
 x = float(input("Введите x "))
 y = float(input("Введите y "))
 z = float(input("Введите z "))
-s = math.log(y**(-(abs(x)**0.5))) * (x-y/2) + math.sin(math.atan(z))**2
-print("s = {0:.3f}".format(s))
+s = (((9+(x-y)**2)**1/3)/x**2+y**2+2)-math.exp(abs(x-y))*(math.tan(z))**3
+print("s = {0:.6f}".format(s))
 
 
